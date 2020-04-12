213e7408183acb78a503998ba6854d437d911d0b
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-04-12 19:51:37 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "master" ]; then
     .
   exit $?
 fi
-echo $DOCKER_PASSWORD | docker login -u qmcgaw --password-stdin &> /dev/null
+echo $DOCKER_PASSWORD | docker login -u dockerpirate --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
      --progress plain \
